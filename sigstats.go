a8cadfdb82149138a01c81959a11580147380d0f
Max Riveiro <kavu13@gmail.com>
2013-07-06 22:38:24 +0400

    Remove code example in docstring completly

--- sigstats.go
+++ sigstats.go
@@ -5,19 +5,6 @@
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 // This package is just a tiny handler function for printing some statistics (mostly memory) for running application with just a tiny call of SIGINFO - ^T shortcut.
-//
-// Example
-//
-// // Let's enable all the Stats
-// sigstats.EnableSigStats(&sigstats.SigStatsSettings{
-//   NumCPU:       true,
-//   NumCGOCalls:  true,
-//   NumGoroutine: true,
-//   MemStats:     true,
-//   GoVersion:    true,
-//   GCStats:      true,
-// })
-// select {}
 package sigstats
 
 import (
