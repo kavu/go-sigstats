040f9ed9f1f7aae1b5ec5118a3640a02340605c2
Max Riveiro <kavu13@gmail.com>
2013-07-06 22:15:43 +0400

    Delete doc.go

--- sigstats.go
+++ sigstats.go
@@ -4,6 +4,23 @@
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+// This package is just a tiny handler function for printing some statistics (mostly memory) for running application with just a tiny call of SIGINFO - ^T shortcut.
+//
+// Example
+//
+// func main() {
+//   // Let's enable all the Stats
+//   sigstats.EnableSigStats(&sigstats.SigStatsSettings{
+//     NumCPU:       true,
+//     NumCGOCalls:  true,
+//     NumGoroutine: true,
+//     MemStats:     true,
+//     GoVersion:    true,
+//     GCStats:      true,
+//   })
+//   select {}
+// }
+
 package sigstats
 
 import (
